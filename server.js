9b90720379b156285cf4677546563566b477b4f8
unknown <ahmedomar1007@gmail.com>
2022-06-06 17:26:39 +0200

    parse csv

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-import parse from 'csv-parse'
+import { parse } from 'csv-parse'
 import fs from 'fs'
 
-const results = []
+const habitablePlanets = []
 
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  )
+}
 fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
   .on('data', (data) => {
-    results.push(data)
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data)
+    }
+  })
+  .on('error', (err) => {
+    console.log(err)
   })
   .on('end', () => {
-    console.log(results)
+    console.log(
+      habitablePlanets.map((planets) => {
+        return planets['kepler_name']
+      })
+    )
+    console.log(`${habitablePlanets.length} Habitable Planets found!`)
     console.log('Done!')
   })
